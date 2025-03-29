e44682595e1b9910f2a2a6a289c413a73db15de7
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-29 17:39:53 -0300

    basic get with path parameter and query parameters

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 BOOKS = [
     {"title": "Title One", "author": "Author One", "category": "science"},
-    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Two", "author": "Author One", "category": "science"},
     {"title": "Title Three", "author": "Author Three", "category": "history"},
     {"title": "Title Four", "author": "Author Four", "category": "math"},
     {"title": "Title Five", "author": "Author Five", "category": "math"},
@@ -17,9 +19,28 @@ async def books():
     return BOOKS
 
 
-@app.get("/books/{book_title}")
-async def book(book_title: str):
-    for book in BOOKS:
-        if book.get("title", "").casefold() == book_title.casefold():
-            return book
-    return {"title": None, "author": None, "category": None}
+@app.get("/books/category/{book_category}")
+async def book(book_category: str, title: str | None = None, author: str | None = None):
+    books_to_return: list[dict[str, Any]] = []
+    books_to_return.extend(
+        book
+        for book in BOOKS
+        if book.get("category", "").casefold() == book_category.casefold()
+    )
+    if not books_to_return:
+        return {"title": None, "author": None, "category": None}
+    if books_to_return and title:
+        books_to_return = [
+            book
+            for book in books_to_return
+            if book.get("title", "").casefold() == title.casefold()
+        ]
+    if books_to_return and author:
+        books_to_return = [
+            book
+            for book in books_to_return
+            if book.get("author", "").casefold() == author.casefold()
+        ]
+    if not books_to_return:
+        return {"title": None, "author": None, "category": None}
+    return books_to_return
