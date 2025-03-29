6e7e28f67a3de0d58e68025e378ea5c18faf88ea
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-29 18:25:04 -0300

    generalization for get data with params and queries

--- main.py
+++ main.py
@@ -14,44 +14,80 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
-async def books():
-    return BOOKS
-
-
-@app.get("/books/category/{book_category}")
-async def book(book_category: str, title: str | None = None, author: str | None = None):
+def fetch_by(param: dict[str, Any], query: list[dict[str, Any] | None] = None):
     books_to_return: list[dict[str, Any]] = []
     books_to_return.extend(
         book
         for book in BOOKS
-        if book.get("category", "").casefold() == book_category.casefold()
+        if book.get(param["name"], "").casefold() == param["value"].casefold()
     )
     if not books_to_return:
         return {"title": None, "author": None, "category": None}
-    if books_to_return and title:
-        books_to_return = [
-            book
-            for book in books_to_return
-            if book.get("title", "").casefold() == title.casefold()
-        ]
-    if books_to_return and author:
-        books_to_return = [
-            book
-            for book in books_to_return
-            if book.get("author", "").casefold() == author.casefold()
-        ]
+    if not query:
+        return books_to_return
+    for query_param in query:
+        if books_to_return and query_param:
+            books_to_return = [
+                book
+                for book in books_to_return
+                if book.get(query_param["name"], "").casefold()
+                == query_param["value"].casefold()
+            ]
     if not books_to_return:
         return {"title": None, "author": None, "category": None}
     return books_to_return
 
 
-@app.post("/books/create_book")
+@app.get("/books")
+async def books():
+    return BOOKS
+
+
+@app.get("/books/category/{category}")
+async def books_by_category(
+    category: str, title: str | None = None, author: str | None = None
+):
+    return fetch_by(
+        {"name": "category", "value": category},
+        [
+            {"name": "title", "value": title} if title else None,
+            {"name": "author", "value": author} if author else None,
+        ],
+    )
+
+
+@app.get("/books/title/{title}")
+async def books_by_title(
+    title: str, category: str | None = None, author: str | None = None
+):
+    return fetch_by(
+        {"name": "title", "value": title},
+        [
+            {"name": "category", "value": category} if category else None,
+            {"name": "author", "value": author} if author else None,
+        ],
+    )
+
+
+@app.get("/books/author/{author}")
+async def books_by_author(
+    author: str, category: str | None = None, title: str | None = None
+):
+    return fetch_by(
+        {"name": "author", "value": author},
+        [
+            {"name": "category", "value": category} if category else None,
+            {"name": "title", "value": title} if title else None,
+        ],
+    )
+
+
+@app.post("/books/create")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
 
 
-@app.put("/books/update_book")
+@app.put("/books/update")
 async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i].get("title", "").casefold() == updated_book.get("title").casefold():
@@ -59,7 +95,7 @@ async def update_book(updated_book=Body()):
             break
 
 
-@app.delete("/books/delete_book/{book_title}")
+@app.delete("/books/delete/{book_title}")
 async def delete_book(book_title: str):
     for i in range(len(BOOKS)):
         if BOOKS[i].get("title", "").casefold() == book_title.casefold():
